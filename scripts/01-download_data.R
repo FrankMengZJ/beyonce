53fe9f329e8ab850a8c3107a84e7cea8cc2346e0
Frank Meng <2460810133frank@gmail.com>
2024-02-29 10:07:45 -0500

    Updated

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,21 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(usethis)
+library(spotifyr)
 library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
-
+get_spotify_access_token()
+beyonce <- get_artist_audio_features("beyonce")
+saveRDS(beyonce, "data/raw_data/beyonce.rds")
 
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
